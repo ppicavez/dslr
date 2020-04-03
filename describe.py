0c7e9b503667b650ae1e04d656061246ed74ded2
Pascal PICAVEZ <pascal.picavez@gmail.com>
2020-04-03 15:38:27 +0200

    Lecture données

--- describe.py
+++ describe.py
@@ -1,6 +1,12 @@
 import sys
+import csv
+import pandas as pd
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise "Usage: {} data_file".format(sys.argv[0])
 
+    with open('test.csv', 'r') as csvfile:
+        dataReader = csv.reader(csvfile, delimiter=',')
+
+    df = pd.read_csv(sys.argv[1])
